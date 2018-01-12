4076bea439e66fb85aa9b13e60b049525c9afc5d
D-Bits <D-Bits@users.noreply.github.com>
2018-01-11 17:31:56 -0800

    Add files via upload
    First Look

---
+++ Controllers/MailingController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AspAssignments.Models;
+
+namespace AspAssignments.Controllers
+{
+    public class MailingController : Controller
+    {
+        // GET: Mailing
+        public ActionResult Index()
+        {
+            Mailing m1 = new Mailing();
+            m1.FirstName = "James";
+            m1.LastName = "Lahey";
+            m1.Email = "jlahey@sunnyvale.com";
+
+            Mailing m2 = new Mailing();
+            m2.FirstName = "Randy";
+            m2.LastName = "Bobandy";
+            m2.Email = "ilovecheeseburgers@sunnyvale.com";
+
+            Mailing m3 = new Mailing();
+            m3.FirstName = "George";
+            m3.LastName = "Green";
+            m3.Email = "dumbcop@dartmouthpd.ca";
+
+            List<Mailing> mailings = new List<Mailing>();
+            mailings.Add(m1);
+            mailings.Add(m2);
+            mailings.Add(m3);
+
+            return View(mailings);
+        }
+    }
+}
